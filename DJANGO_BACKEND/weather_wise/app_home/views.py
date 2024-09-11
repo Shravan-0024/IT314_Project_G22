0a34ed7b0ea3bcdd4ff7b3947d73f42118a3fd86
MADHAV JIVANI <202201285@daiict.ac.in>
2024-09-11 13:53:22 +0530

    Enable Signup functions use custom UserSignUp configured in forms.py using built-in django's UserCreationForm also displaying form on signup.html and test if user is signed up

--- DJANGO_BACKEND/weather_wise/app_home/views.py
+++ DJANGO_BACKEND/weather_wise/app_home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from app_home.forms import UserSignUpForm
+from django.contrib.auth import login
 
 def home_view(request):
     return render(request,'home/home.html')
@@ -10,7 +12,17 @@ def login_view(request):
     return render(request,'registration/login.html')
 
 def signup_view(request):
-    return render(request,'registration/signup.html')
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('home_view')
+    else:
+        form = UserSignUpForm()
+        return render(request, 'registration/signup.html', {'form': form})
 
 def predict_view(request):
     return render(request,'home/predict.html')
