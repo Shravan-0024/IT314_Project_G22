ad1b52bc2ae1bee3d534e447af1693ccd0657ffc
MADHAV JIVANI <202201285@daiict.ac.in>
2024-09-11 10:40:10 +0530

    Loading Bootstrap for development purpose and create some templates to render and assign views and urls to few of them

--- DJANGO_BACKEND/weather_wise/app_home/urls.py
+++ DJANGO_BACKEND/weather_wise/app_home/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from app_home import views
 urlpatterns = [
-    path('',views.home_view,name="home_view")
+    path('',views.home_view,name="home_view"),
+    path('about/',views.about_view,name="about_view"),
 ]
