035bb609b0ced2cd7da270322bd05767b0de3a0b
Shravan-0024 <202201333@daiict.ac.in>
2024-10-24 14:15:20 +0530

    Notify_model

--- DJANGO_BACKEND/weather_wise/app_home/models.py
+++ DJANGO_BACKEND/weather_wise/app_home/models.py
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 from django.db import models
+=======
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notify(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferred_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Notify preferences for {self.user.username}"
+>>>>>>> 256e932 (Notify_model)
