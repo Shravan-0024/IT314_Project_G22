f58f7cce3a3950d4930ae9618d5c1fc20bddc9ae
Shravan-0024 <202201333@daiict.ac.in>
2024-10-24 14:29:08 +0530

    Updated Notify

--- DJANGO_BACKEND/weather_wise/app_home/models.py
+++ DJANGO_BACKEND/weather_wise/app_home/models.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 from django.db import models
-=======
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,4 +8,4 @@ class Notify(models.Model):
 
     def __str__(self):
         return f"Notify preferences for {self.user.username}"
->>>>>>> 256e932 (Notify_model)
+
