dc6fbdca001c9e5fad70bba31dba0c6b7c6193f7
Sumit Vishwakarma <202201320@daiict.ac.in>
2024-11-14 19:04:24 +0530

    updated models.py in app_home

--- DJANGO_BACKEND/weather_wise/app_home/models.py
+++ DJANGO_BACKEND/weather_wise/app_home/models.py
@@ -7,3 +7,15 @@ class Notify(models.Model):
 
     def __str__(self):
         return f"Notify preferences for {self.user.username}"
+
+class Feedback(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)  # Optional user association
+    predictions_accuracy = models.TextField(null=True, blank=True)  # Stores the selected answers as a text list
+    app_usability = models.TextField(null=True, blank=True)
+    user_interface = models.TextField(null=True, blank=True)
+    helpful_info = models.TextField(null=True, blank=True)
+    app_recommend = models.TextField(null=True, blank=True)
+ 
+
+    def __str__(self):
+        return f"Feedback from {self.user}" if self.user else "Anonymous Feedback"
