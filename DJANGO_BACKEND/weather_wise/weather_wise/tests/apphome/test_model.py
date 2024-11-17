b6003c19ef683aede50bd8f70ed4eb93aa917d6a
Sumit-320 <202201320@daiict.ac.in>
2024-11-17 12:28:15 +0530

    favorite_loc model unit testing done

--- DJANGO_BACKEND/weather_wise/weather_wise/tests/apphome/test_model.py
+++ DJANGO_BACKEND/weather_wise/weather_wise/tests/apphome/test_model.py
@@ -1,13 +1,13 @@
 import pytest
 from django.contrib.auth.models import User
-from app_home.models import Feedback, Notify
+from app_home.models import Feedback, Notify, Fav_loc
 from django.core.exceptions import ValidationError
 
-
+print("Sumit testing: Feedback, Notify and Favorite Locations Models")
 @pytest.fixture
 def user():
     # creating a test user
-    return User.objects.create_user(username='testuser', password='password')
+    return User.objects.create_user(username='Sumit', password='sumit_is_testing')
 
 
 @pytest.fixture
@@ -34,8 +34,8 @@ def notify(user):
 
 @pytest.mark.django_db 
 def test_feedback_creation(feedback):
-    # Test that a Feedback instance is created successfully
-    assert feedback.user.username == 'testuser'
+    # to test that a Feedback instance is created successfully
+    assert feedback.user.username == 'Sumit'
     assert feedback.predictions_accuracy == 'accurate'
     assert feedback.app_usability == 'easy'
     assert feedback.user_interface == 'intuitive'
@@ -45,7 +45,7 @@ def test_feedback_creation(feedback):
 
 @pytest.mark.django_db 
 def test_null_fields(user):
-    # Test that empty fields are not saved in the database
+    # to test that empty fields are not saved in the database
     feedback = Feedback.objects.create(user=user, predictions_accuracy='accurate')
     assert feedback.app_usability is None
     assert feedback.user_interface is None
@@ -55,18 +55,18 @@ def test_null_fields(user):
 
 @pytest.mark.django_db  
 def test_feedback_str(feedback):
-    # Test the __str__ method for the Feedback model
+    # to test the __str__ method for the Feedback model
     assert str(feedback) == f"Feedback from {feedback.user.username}"
 
-    # Case 2: Feedback has no associated user
+    # to test that feedback model has no associated user
     feedback_anonymous = Feedback.objects.create(user=None, predictions_accuracy='accurate')
     assert str(feedback_anonymous) == "Anonymous Feedback"
 
 
 @pytest.mark.django_db  
 def test_notify_creation(notify):
-    # Test that a Notify instance is created successfully
-    assert notify.user.username == 'testuser'
+    # to test that a Notify instance is created successfully
+    assert notify.user.username == 'Sumit'
     assert notify.preferred_location == 'Mumbai'
 
 
@@ -93,22 +93,22 @@ def test_preferred_location_max_length(user, location, expected):
 @pytest.mark.django_db  
 @pytest.mark.xfail(reason="Expected to fail if user has more than one Notify instance")
 def test_unique_user(user, notify):
-    # Testcase for a user to have only one Notify instance
+    # to test for a user to have only one Notify instance
     Notify.objects.create(user=user, preferred_location='Ahmedabad')
     assert Notify.objects.count() == 1  # Only one Notify instance should exist
 
 
 @pytest.mark.django_db
 def test_notify_str(notify):
-    # Testcase for testing the __str__ method for the Notify model
+    # to test the __str__ method for the Notify model
     assert str(notify) == f"Notify preferences for {notify.user.username}"
 
 
 
 @pytest.mark.django_db 
 @pytest.mark.parametrize("invalid_value", ['invalid_choice', 'unknown', 'untrusted'])
-def test_feedback_app_recommend_choices(feedback, invalid_value):
-    # Tests that 'app_recommend' field has valid choices
+def test_feedback_choices(feedback, invalid_value):
+    # to test that 'app_recommend' field has valid choices
     valid_choices = ['yes', 'no', 'maybe']
     
     feedback.app_recommend = invalid_value
@@ -118,11 +118,63 @@ def test_feedback_app_recommend_choices(feedback, invalid_value):
 
 @pytest.mark.django_db  
 def test_notify_user_preference(user):
-    # Test that Notify instance respects the user preference location
+    # tp test that Notify instance follows the user preference location
     notify = Notify.objects.create(user=user, preferred_location='Bangalore')
     assert notify.preferred_location == 'Bangalore'
 
-    # Test updating the preferred location
+    #  to test updating the preferred location
     notify.preferred_location = 'Delhi'
     notify.save()
     assert notify.preferred_location == 'Delhi'
+
+
+# Below are the testcases for Favorite Locations Model
+@pytest.fixture
+def user():
+    # Create a test user
+    return User.objects.create_user(username="Sumit", email="Sumit@example.com", password="sumit_is_testing")
+
+
+@pytest.mark.django_db
+def test_valid_fav_loc_creation(user):
+    # to test the creation of a valid Fav_loc instance
+    fav_loc = Fav_loc.objects.create(user=user, favourite_location="Mumbai")
+    assert Fav_loc.objects.count() == 1
+    assert fav_loc.favourite_location == "Mumbai"
+    assert fav_loc.user == user
+
+@pytest.mark.xfail(reason="creating fav_loc with empty favourite_location is expected to fail!")
+@pytest.mark.django_db
+def test_invalid_empty_fav_loc(user):
+    # to test that creating a fav_loc instance with an empty string fails
+    fav_loc = Fav_loc(user=user, favourite_location="")
+    if fav_loc.favourite_location == "": #I intentionally entered empty field!
+        raise ValidationError("Favourite location cannot be empty.")
+
+@pytest.mark.xfail(reason="entering favorite location string with more than 100 chars is expected to fail!")
+@pytest.mark.django_db
+def test_fav_loc_max_length(user):
+    long_location = "Gandhinagar"*101  # I intentionally exceeded max_length of 100
+    fav_loc = Fav_loc(user=user, favourite_location=long_location) 
+    if len(long_location) > 100:
+        raise ValidationError("Favourite location string cant exceed 100 chars!")
+
+
+@pytest.mark.django_db
+def test_valid_multiple_fav_locs(user):
+    # to test that a user can have multiple favorite locations
+    fav_loc1 = Fav_loc.objects.create(user=user, favourite_location="Shimla")
+    fav_loc2 = Fav_loc.objects.create(user=user, favourite_location="Varanasi")
+    
+    assert Fav_loc.objects.count() == 2
+    assert fav_loc1.favourite_location == "Shimla"
+    assert fav_loc2.favourite_location == "Varanasi"
+    assert fav_loc1.user == user
+    assert fav_loc2.user == user
+
+
+@pytest.mark.django_db
+def test_fav_loc_str(user):
+    # to test the __str__ method of Fav_loc model
+    fav_loc = Fav_loc.objects.create(user=user, favourite_location="Lucknow")
+    assert str(fav_loc) == f"Favourite location for {user.username}"
